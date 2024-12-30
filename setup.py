6f7aa33816008862f3877bb84f5ef4604f2dcd17
Abdul Qoyyuum <aqoyyuum@cardaccess.com.au>
2024-12-30 15:39:24 +0800

    :fire: long_description stuff

--- setup.py
+++ setup.py
@@ -1,7 +1,3 @@
 from setuptools import setup
-from pathlib import Path
 
-setup(
-    long_description=Path("README.rst").read_text(),
-    long_description_content_type = "text/restructuredtext"
-)
+setup()
