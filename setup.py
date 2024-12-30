9068410954baa648e91266ba37e1d203905dcde6
Abdul Qoyyuum <aqoyyuum@cardaccess.com.au>
2024-12-30 15:35:36 +0800

    :memo: Fixed long description stuff :rocket:

--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
 from setuptools import setup
+from pathlib import Path
 
-setup()
+setup(
+    long_description=Path("README.rst").read_text(),
+    long_description_content_type = "text/restructuredtext"
+)
